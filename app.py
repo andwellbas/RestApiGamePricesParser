e4c6556d11389868b407c7dde64673cb9899a5a1
andwellbas <basanetand@gmail.com>
2023-02-17 15:38:04 +0200

    second commit

--- app.py
+++ app.py
@@ -15,12 +15,17 @@ def json_game_prices():
             if 'Game Name' in request_data:
                 game_name = request_data['Game Name']
 
-        return find_prices.find_game_prices(game_name)
+        if game_name == "":
+            return {"Error": "Missing game name."}
+        else:
+            return find_prices.find_game_prices(game_name)
 
     except IndexError:
         return {"Index Error": "Game name must be a string."}
     except TypeError:
         return {"Type Error": "Game name must be a string."}
+    except AttributeError:
+        return {"AttributeError": "Game name must be a string."}
 
 
 if __name__ == '__main__':
